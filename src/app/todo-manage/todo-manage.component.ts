2dbb7d9406313b963e58d5b43329379c18843992
Angkarn Janjuang <angkarn9@gmail.com>
2019-08-30 20:55:14 +0700

    initial project

--- src/app/todo-manage/todo-manage.component.ts
+++ src/app/todo-manage/todo-manage.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-manage',
@@ -7,50 +6,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./todo-manage.component.scss']
 })
 export class TodoManageComponent implements OnInit {
-  todoForm: FormGroup = new FormGroup({
-    topic: new FormControl('', [Validators.required]),
-    description: new FormControl('', [Validators.required]),
-  });
-  todoList = [];
-  isAdd = true;
-  selectedTodoItem: number;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  add() {
-    this.todoList.push(this.todoForm.getRawValue());
-    this.reset();
-  }
-
-  edit(index: number) {
-    this.isAdd = false;
-    // const topic = this.todoList[index].topic;
-    // const description = this.todoList[index].description;
-    // this.todoForm.controls.topic.setValue(topic);
-    // this.todoForm.controls.description.setValue(description);
-    this.todoForm.setValue(this.todoList[index]);
-    this.selectedTodoItem = index;
-  }
-
-  update() {
-    this.isAdd = true;
-    this.todoList[this.selectedTodoItem] = this.todoForm.getRawValue();
-    this.reset();
-  }
-
-  remove(index: number) {
-    this.isAdd = true;
-    delete this.todoList[index];
-    this.todoList = this.todoList.filter(e => e);
-    this.reset();
-  }
-
-  reset() {
-    this.todoForm.controls.topic.reset('');
-    this.todoForm.controls.description.reset('');
   }
 
 }
